f37d1c65d67280062a488871f45a343b2ed11015
Sean Horgan <seanhorgan98@gmail.com>
2019-09-30 22:59:37 +0100

    Basic SHA hashing testing

--- src/main.java
+++ src/main.java
@@ -5,11 +5,43 @@
 import Block.java;
 import Blockchain.java;
 import java.time.Instant;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.math.BigInteger;
 
 class Main{
     public static void main(String[] args) {
-        Blockchain blockChain = new Blockchain();
-        System.out.println("Welcome to the Thunderdome!");
+        System.out.println("-----------------------------------------");
+        //1. Create Genisis Block
+        //2. Create blockchain
+        
+        
+        String hashInput = "Hello World";
+
+        System.out.println("Input: " + hashInput);
+        System.out.println("Output: " + getSHAString(hashInput));
     }
+
+    public static String getSHAString(String input){
+        BigInteger number = new BigInteger(1, getSHA(input));
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+        while(hexString.length() < 32){
+            hexString.insert(0, '0');
+        }
+        return hexString.toString();
+    }
+
+    public static byte[] getSHA(String input){
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            return md.digest(input.getBytes(StandardCharsets.UTF_8)); 
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("No Such algorithm");
+            return null;
+        }
+        
+    }
+
 
 }
