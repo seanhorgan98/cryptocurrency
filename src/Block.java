6957788fd555319e3e2976b64a4882edbfc173aa
Sean Horgan <seanhorgan98@gmail.com>
2019-09-30 22:26:56 +0100

    Creation of Block, Blockchain, and main classes.

---
+++ src/Block.java
@@ -0,0 +1,19 @@
+import java.time.Instant;
+
+class Block {
+    public final int index;
+    public final Long timeStamp;
+    //previousHash
+    //data
+    //hash
+
+    public Block(int index, Long timeStamp){
+        this.index = index;
+        this.timeStamp = timeStamp;
+    }
+
+    public void addTransaction(){
+
+    }
+
+}
---
+++ src/main.java
@@ -0,0 +1,15 @@
+/**
+ * Main Class used for testing the blockchain and blocks.
+ */
+
+import Block.java;
+import Blockchain.java;
+import java.time.Instant;
+
+class Main{
+    public static void main(String[] args) {
+        Blockchain blockChain = new Blockchain();
+        System.out.println("Welcome to the Thunderdome!");
+    }
+
+}
