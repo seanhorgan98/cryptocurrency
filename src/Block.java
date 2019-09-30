1169e793bb9b58beac04cad2848ffb7d679a7925
Sean Horgan <seanhorgan98@gmail.com>
2019-09-30 23:18:26 +0100

    Block creation and initialisation

--- src/Block.java
+++ src/Block.java
@@ -1,19 +1,46 @@
 import java.time.Instant;
+import java.security.MessageDigest;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.math.BigInteger;
 
 class Block {
     public final int index;
     public final Long timeStamp;
-    //previousHash
-    //data
-    //hash
+    public final String previousHash;
+    public final String data;
+    public final String hash;
 
-    public Block(int index, Long timeStamp){
+    public Block(int index, Long timeStamp, String previousHash, String data){
         this.index = index;
         this.timeStamp = timeStamp;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.hash = getSHAString(Integer.toString(index) + previousHash + timeStamp.toString() + data);;
     }
 
     public void addTransaction(){
 
+    }
+
+    private static String getSHAString(String input){
+        BigInteger number = new BigInteger(1, getSHA(input));
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+        while(hexString.length() < 32){
+            hexString.insert(0, '0');
+        }
+        return hexString.toString();
+    }
+
+    private static byte[] getSHA(String input){
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            return md.digest(input.getBytes(StandardCharsets.UTF_8)); 
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("No Such algorithm");
+            return null;
+        }
+        
     }
 
 }
--- src/main.java
+++ src/main.java
@@ -5,43 +5,15 @@
 import Block.java;
 import Blockchain.java;
 import java.time.Instant;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.math.BigInteger;
 
 class Main{
     public static void main(String[] args) {
         System.out.println("-----------------------------------------");
         //1. Create Genisis Block
         //2. Create blockchain
+        Long unixTime = Instant.now().getEpochSecond();
+        Block firstBlock = new Block(1, unixTime, "previousBlockHash", "new data!");
         
-        
-        String hashInput = "Hello World";
-
-        System.out.println("Input: " + hashInput);
-        System.out.println("Output: " + getSHAString(hashInput));
+        System.out.println("Block Info: " + firstBlock.hash);
     }
-
-    public static String getSHAString(String input){
-        BigInteger number = new BigInteger(1, getSHA(input));
-        StringBuilder hexString = new StringBuilder(number.toString(16));
-        while(hexString.length() < 32){
-            hexString.insert(0, '0');
-        }
-        return hexString.toString();
-    }
-
-    public static byte[] getSHA(String input){
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            return md.digest(input.getBytes(StandardCharsets.UTF_8)); 
-        } catch (NoSuchAlgorithmException e) {
-            System.out.println("No Such algorithm");
-            return null;
-        }
-        
-    }
-
-
 }
