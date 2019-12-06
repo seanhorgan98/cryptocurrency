d975ed790bf3fb314006186a8cbdcdd004d773f0
Sean Horgan <seanhorgan98@gmail.com>
2019-12-06 17:06:15 +0000

    Updated wallet

--- src/main/Wallet.java
+++ src/main/Wallet.java
@@ -3,6 +3,7 @@ package main;
 import java.security.*;
 import java.security.spec.ECGenParameterSpec;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.ArrayList;
 
 public class Wallet {
@@ -10,6 +11,8 @@ public class Wallet {
 	public PrivateKey privateKey;
 	public PublicKey publicKey;
 	private float balance;
+
+	public HashMap<String,TransactionOutput> UTXOs = new HashMap<String,TransactionOutput>();
 	
 	public Wallet(){
 		generateKeyPair();
@@ -17,8 +20,19 @@ public class Wallet {
 	}
 
 	public float getBalance(){
-		updateWallet();
+		updateBalance();
 		return balance;
+	}
+	public void updateBalance(){
+		float total = 0;	
+        for (Map.Entry<String, TransactionOutput> item: Blockchain.UTXOs.entrySet()){
+        	TransactionOutput UTXO = item.getValue();
+            if(UTXO.isMine(publicKey)) { //if output belongs to me ( if coins belong to me )
+            	UTXOs.put(UTXO.id,UTXO); //add it to our list of unspent transactions.
+            	total += UTXO.value ; 
+            }
+		}
+		balance = total;
 	}
 		
 	public void generateKeyPair() {
@@ -37,15 +51,6 @@ public class Wallet {
 		}
 	}
 
-	public void updateWallet(){
-		//Loop through UTXOs and update balance where hashs match this wallet
-		for (Map.Entry<String, TransactionOutput> entry : Blockchain.UTXOs.entrySet()) {
-			if(StringUtil.getStringFromKey(entry.getValue().reciepient)  == StringUtil.getStringFromKey(publicKey)){
-				balance += entry.getValue().value;
-			}
-		}
-	}
-
 	//Client side check to see if wallet has enough coins to make transaction
 	public boolean sufficientFunds(Transaction tx){
 		if(balance < tx.value){
@@ -55,11 +60,42 @@ public class Wallet {
 		return true;
 	}
 
+	public void printUTXOs(){
+		updateBalance();
+		System.out.println("UTXOs size: " + UTXOs.size());
+		for (Map.Entry<String, TransactionOutput> item: UTXOs.entrySet()){
+			System.out.println(item.getValue().value);
+		}
+	}
 
-	public Transaction createTransaction(PublicKey recipient, float value, ArrayList<TransactionInput> inputs){
+
+	public Transaction sendFunds(PublicKey recipient, float value){
+		if(getBalance() < value){
+			System.out.println("Wallet: Not enough funds!");
+			return null;
+		}
+
+		ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+		
+		float total = 0;
+		for (Map.Entry<String, TransactionOutput> item: UTXOs.entrySet()){
+			TransactionOutput UTXO = item.getValue();
+			total += UTXO.value;
+			
+			inputs.add(new TransactionInput(UTXO.id, UTXO.value));
+
+			if(total > value) break;
+		}
+	
+		
 		Transaction tx = new Transaction(publicKey, recipient, value, inputs);
 		tx.outputs.add(new TransactionOutput(tx.reciepient, tx.value, tx.transactionId));
 		tx.generateSignature(privateKey);
+
+		for(TransactionInput input: inputs){
+			UTXOs.remove(input.previousOutId);
+		}
+		//System.out.println("Sent from: " + StringUtil.getStringFromKey(publicKey) + ", to: " + StringUtil.getStringFromKey(recipient));
 		return tx;
 	}
 	
