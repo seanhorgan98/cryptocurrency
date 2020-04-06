0cb4e504fa9dd6547b5b6e72cc7a79d61ac7fa10
Sean Horgan <seanhorgan98@gmail.com>
2020-04-06 01:22:54 +0100

    Final Changes to test harness

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,7 @@ import java.util.Random;
 class Main {
     public static void main(String[] args) {
         System.out.println("\n---------OUTPUT------------");
-        final int TEST_LENGTH = 1;
+        final int TEST_LENGTH = 10;
 
         //Create Genesis Block
         Block genesisBlock = Block.buildGenesisBlock();
@@ -59,22 +59,13 @@ class Main {
             //Create a transaction from two random wallets
             int wallet1 = r.nextInt(10);
             int wallet2 = r.nextInt(10);
+            //Make sure wallets are not the same
             while(wallet2 == wallet1){
                 wallet2 = r.nextInt(10);
             }
             float value = r.nextInt(10);
             walletList.get(wallet1).sendFunds(walletList.get(wallet2).publicKey, value);
         }
-
-        // walletList.get(1).sendFunds(walletList.get(2).publicKey, 5);
-        // walletList.get(2).sendFunds(walletList.get(1).publicKey, 10);
-
-        // walletList.get(1).sendFunds(walletList.get(3).publicKey, 50);
-        // walletList.get(2).sendFunds(walletList.get(1).publicKey, 10);
-
-        System.out.println("RANDOM WALLET BALANCE: " + walletList.get(3).getBalance());
-
-
         
         System.out.println("\n---------BLOCKCHAIN--------");
         System.out.println("Valid Blockchain: " + Boolean.toString(blockchain.validateChain()));
@@ -82,18 +73,19 @@ class Main {
 
         
         System.out.println("\n---------BALANCE-----------");
-        System.out.println("Wallet 1: " + walletList.get(1).getBalance());
-        System.out.println("Wallet 2: " + walletList.get(2).getBalance());
+        for(int i = 0; i < walletList.size(); i++){
+            System.out.println("Wallet " + i + ": " + walletList.get(i).getBalance());
+        }
         
 
-        System.out.println("\n---------UTXOs--------");
-        System.out.println("Wallet 1 hash: " + StringUtil.getStringFromKey(walletList.get(1).publicKey));
-        System.out.println("Wallet 1 UTXOs: "); walletList.get(1).printUTXOs();
+        // System.out.println("\n---------UTXOs--------");
+        // System.out.println("Wallet 1 hash: " + StringUtil.getStringFromKey(walletList.get(1).publicKey));
+        // System.out.println("Wallet 1 UTXOs: "); walletList.get(1).printUTXOs();
 
-        System.out.println("\nWallet 2 hash: " + StringUtil.getStringFromKey(walletList.get(2).publicKey));
-        System.out.println("Wallet 2 UTXOs: "); walletList.get(2).printUTXOs();
+        // System.out.println("\nWallet 2 hash: " + StringUtil.getStringFromKey(walletList.get(2).publicKey));
+        // System.out.println("Wallet 2 UTXOs: "); walletList.get(2).printUTXOs();
 
-        //blockchain.printTransactions();
+        blockchain.printTransactions();
 
     }
 }
